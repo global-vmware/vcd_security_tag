4111b1e75b094154e8fa9f367c03b390661e7f66
Matt Scafe <matt.scafe@rackspace.com>
2023-05-16 18:34:50 -0500

    Removed the security_tags and vm_names default values in the variables.tf.  Moved the vcd_vm Data Source above the vcd_security_tag Resource Block in the main.tf for consistency purposes.  Updated the README file to reflect the committed changes.

--- main.tf
+++ main.tf
@@ -9,15 +9,15 @@ terraform {
   }
 }
 
+data "vcd_vm" "vm" {
+  for_each = toset(flatten(values(var.security_tags)))
+
+  name = each.value
+}
+
 resource "vcd_security_tag" "security_tags" {
   for_each = var.security_tags
 
   name   = each.key
   vm_ids = [for vm_name in each.value : data.vcd_vm.vm[vm_name].id]
-}
-
-data "vcd_vm" "vm" {
-  for_each = toset(flatten(values(var.security_tags)))
-
-  name = each.value
 }
